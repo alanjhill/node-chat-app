1f5ce5f5e8e67e12546497244e125ec60a60057c
Alan Hill <ahill@2paths.com>
2017-11-07 13:41:06 -0800

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log("Connected to server");
-
-  socket.emit('createMessage', {
-    from: "me", 
-    text: "Hello"}
-  );
 });
 
 socket.on('newMessage', function(message) {
--- server/server.js
+++ server/server.js
@@ -28,10 +28,10 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message) => {
     console.log("createMessage", message)
   
-    socket.emit('newMessage', {
+    io.emit('newMessage', {
       from: message.from,
       text: message.text,
-      createdAt: new Date()
+      createdAt: new Date().getTime()
     });
   });
 });
