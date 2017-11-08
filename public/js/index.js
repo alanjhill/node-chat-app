15494504fc2bc39274ddf547683efbddefe30c3e
Alan Hill <ahill@2paths.com>
2017-11-07 17:44:00 -0800

    Add form for sending messages and show incoming messages in list

--- public/js/index.js
+++ public/js/index.js
@@ -6,8 +6,28 @@ socket.on('connect', function() {
 
 socket.on('newMessage', function(message) {
   console.log("newMessage", message);
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  $('#messages').append(li);
 });
 
 socket.on('disconnect', function() {
   console.log("Disconnected from server");
+});
+
+socket.emit('createMessage', {
+  from: "Frank",
+  text: "Hi"
+}, function(data) {
+  console.log("Got it:", data);
+});
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=text]').val()
+  }, function(data) {
+    console.log("data", data);
+  })
 });
--- server/server.js
+++ server/server.js
@@ -31,12 +31,14 @@ io.on('connection', (socket) => {
   //   createdAt: new Date()
   // })
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log("createMessage", message)
   
     io.emit('newMessage', 
       generateMessage(message.from, message.text)
     );
+
+    callback("This is from the server");
 
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
